63a42fad2728f9b57205d4f54dbb00d036beb689
ZoboZoro <107361388+ZoboZoro@users.noreply.github.com>
2026-01-27 02:37:38 +0000

    pipeline

--- config.py
+++ config.py
@@ -15,26 +15,12 @@ base_url = 'https://github.com/DataTalksClub/nyc-tlc-data/' \
             'releases/download/yellow/'
 url = f'{base_url}yellow_tripdata_2021-07.csv.gz'
 
-dtype = {
-        "VendorID": "Int64",
-        "passenger_count": "Int64",
-        "trip_distance": "float64",
-        "RatecodeID": "Int64",
-        "store_and_fwd_flag": "string",
-        "PULocationID": "Int64",
-        "DOLocationID": "Int64",
-        "payment_type": "Int64",
-        "fare_amount": "float64",
-        "extra": "float64",
-        "mta_tax": "float64",
-        "tip_amount": "float64",
-        "tolls_amount": "float64",
-        "improvement_surcharge": "float64",
-        "total_amount": "float64",
-        "congestion_surcharge": "float64",
-    }
+file_path = './data/green_tripdata_2025-11.parquet'
+lookup_path = './data/taxi_zone_lookup.csv'
 
-parse_dates = [
-        "tpep_pickup_datetime",
-        "tpep_dropoff_datetime"
-    ]
+dtype = {
+        "LocationID": "Int64",
+        "Borough": "string",
+        "Zone": "string",
+        "service_zone": "string",
+    }
