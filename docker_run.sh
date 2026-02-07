38609b913c610825bce4dd9adedc492d5639d743
ZoboZoro <107361388+ZoboZoro@users.noreply.github.com>
2026-02-07 13:19:33 +0000

    scripting

--- docker_run.sh
+++ docker_run.sh
@@ -1,8 +1,8 @@
 # /bin/bash
 
 docker run -it --rm \
-    -e POSTGRES_USER='root' \
-    -e POSTGRES_PASSWORD='root' \
+    -e POSTGRES_USER='sensitive' \
+    -e POSTGRES_PASSWORD='sensitive' \
     -e POSTGRES_DB='taxis' \
     -v taxis_data:/var/lib/postgresql \
     -p 5432:5432 \
